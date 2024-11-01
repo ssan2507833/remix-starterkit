9eb64aa0276d9e9fb3adbd2299a4d579d64cef14
ss12932 <ss12932@my.bristol.ac.uk>
2024-11-01 12:23:32 +0000

    chore: add GitHub Actions workflow for deployment and testing; remove old Playwright workflow

--- app/root.tsx
+++ app/root.tsx
@@ -7,7 +7,7 @@ import {
 } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
 
-import "./tailwind.css";
+import tailwindStylesheetUrl from "./styles/tailwind.css";
 
 export const links: LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -20,18 +20,19 @@ export const links: LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
+  { rel: "stylesheet", href: tailwindStylesheetUrl },
 ];
 
-export function Layout({ children }: { children: React.ReactNode }) {
+export function Document({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`h-full overflow-x-hidden`}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="flex h-full flex-col justify-between bg-background text-foreground">
         {children}
         <ScrollRestoration />
         <Scripts />
@@ -41,5 +42,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return <Outlet />;
+  return (
+    <Document>
+      <Outlet />
+    </Document>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,20 @@
 import type { Config } from "tailwindcss";
+import animatePlugin from "tailwindcss-animate";
 
 export default {
   content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      container: {
+        center: true,
+        padding: "1.5rem",
+        screens: {
+          sm: "100%",
+          md: "100%",
+          lg: "100%",
+          xl: "1280px",
+        },
+      },
       fontFamily: {
         sans: [
           '"Inter"',
@@ -18,5 +29,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [animatePlugin],
 } satisfies Config;
