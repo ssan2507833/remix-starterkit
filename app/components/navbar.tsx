5aa1ecabc6615e6def4bf04832dedcb5d9f36a27
ss12932 <ss12932@my.bristol.ac.uk>
2024-11-01 13:44:32 +0000

    feat: enhance navbar with navigation links

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,7 +1,21 @@
 const Navbar = () => {
-
-  return <div>Navbar test workflow</div>;
-
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
