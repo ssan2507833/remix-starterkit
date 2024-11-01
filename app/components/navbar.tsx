cf54bd4119d95acf0511c0a83010358dbb0d2cc5
S San <ss12932@my.bristol.ac.uk>
2024-11-01 13:58:10 +0000

    feature/navigation bar (#5)
    * feat: initial nav component added to test workflow
    
    * chore: replace old deployment workflow with consolidated main CI workflow
    
    * fix: test workflow
    
    * fix: custom action naming error fixed
    
    * feat: enhance navbar with navigation links
    
    * fix: added some extra links
    
    * Create link.tsx
    
    * fix: added one more i promise
    
    * fix: rectify script for prettier

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,7 +1,33 @@
 const Navbar = () => {
-
-  return <div>Navbar test workflow</div>;
-
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+          <li>
+            <a href="/">Settings</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+          <li>
+            <a href="/contact">Contact2</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
